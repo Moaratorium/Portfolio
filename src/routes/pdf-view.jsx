415ec5fde39c94f3d0afa28a4cb1efa57d02016c
Moaratorium <moaratorium@gmail.com>
2024-10-02 18:25:23 -0500

    pdf view changes

--- src/routes/pdf-view.jsx
+++ src/routes/pdf-view.jsx
@@ -3,8 +3,7 @@ import config from '../../config.json';
 export default function PDFView() {
     return (
         <div id="pdfframe">
-            <h1>{config.datastore.name}'s Resume:</h1>
-            <iframe src={config.datastore.pdfLink} title="environmentPDF" id="pdf-frame"></iframe>
+            <iframe src={config.datastore.pdfLink} title="environmentPDF" id="pdf-frame" style={{height: "70vh"}}></iframe>
         </div>
     )
 };
