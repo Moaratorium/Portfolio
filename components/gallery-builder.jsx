d4f800b2cc0f5d95e988e5d73a523e497885dd19
Moaratorium <moaratorium@gmail.com>
2025-03-14 14:23:51 -0500

    variables accessible in modal fix p1

--- components/gallery-builder.jsx
+++ components/gallery-builder.jsx
@@ -5,17 +5,15 @@ import { useState } from 'react'
 
 export function GalleryBuilder() {
     const galleryList = config.datastore.gallery;
-    let [isOpen, setIsOpen] = useState(false)
     return (
         galleryList.map(item => {
             return (
-                <div key={item.id} id={item.id}>
+                <div key={item.id} id={item.id} className="gallery-container">
                     <img className="project-thumbnail" src={item.thumbnail}></img>
                     <h2 className="project-header">{item.name}<br></br><div className="project-subheader"> role details </div></h2>
                     <p className="project-details" style={{ whiteSpace: 'pre-line' }}>{item.description}</p>
-                    <div className="tag-holder">
-                    <TagMaker tags={item.tagList}/>
-                    <ProjectModal item={item}></ProjectModal>
+                    <div className="modal-holder">
+                    <ProjectModal project={item}></ProjectModal>
                     </div>
                 </div>
             )
--- components/modal.jsx
+++ components/modal.jsx
@@ -1,24 +1,30 @@
 import { Description, Dialog, DialogPanel, DialogTitle } from '@headlessui/react'
 import { useState } from 'react'
+import { TagMaker } from "./tag-maker";
 import config from '../config.json';
 
-export function ProjectModal(item) {
+export function ProjectModal(project) {
+  let galleryList = config.datastore.gallery;
+  let defaultProject = galleryList[0];
   let [isOpen, setIsOpen] = useState(false);
-  let [thisProject, setThisProject] = useState({});
-  function openModal() {
+  let [thisProject, setThisProject] = useState(defaultProject);
+  function openModal(project) {
     setIsOpen(true)
-    setThisProject(item)
+    setThisProject(project)
   }
   return (
     <>
-      <button onClick={() => openModal()}>Details</button>
+      <button className="details-button" onClick={() => openModal(thisProject)}>Details</button>
       <Dialog open={isOpen} onClose={() => setIsOpen(false)} className="modal-overlay">
         <div className="modal-content">
+          <div id="image-pane">
+            some stuff
+          </div>
           <DialogPanel className="dialog-panel">
             <DialogTitle className="font-bold">{thisProject.name}</DialogTitle>
-            <Description>This will permanently deactivate your account</Description>
-            <p>Are you sure you want to deactivate your account? All of your data will be permanently removed.</p>
-            <div className="flex gap-4">
+            <Description>This is where the info goes, below I might put tags instead of on project tab</Description>
+            <TagMaker tags={thisProject.tagList}></TagMaker>
+            <div className="close-button">
               <button onClick={() => setIsOpen(false)}>Close</button>
             </div>
           </DialogPanel>
