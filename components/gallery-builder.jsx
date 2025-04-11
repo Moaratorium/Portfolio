cc3958ae7f6ac274003a4a302520eaed23d7d18d
Moaratorium <moaratorium@gmail.com>
2025-04-11 14:45:06 -0500

    general fit styles, details button -no mobile-

--- components/gallery-builder.jsx
+++ components/gallery-builder.jsx
@@ -8,9 +8,11 @@ export function GalleryBuilder({id, thumbnail, name, images, description, tagLis
         return (
             <>
                 <div key={id} id={id} className="gallery-card">
+                    <div className="project-info-container">
                     <img className="project-thumbnail" src={thumbnail}></img>
                     <h2 className="project-header">{name}</h2>
                     <p className="project-details" style={{ whiteSpace: 'pre-line' }}>{description}</p>
+                    </div>
                     <div className="modal-holder">
                     <button className="details-button" onClick={() => setShowModal(true)}>Details</button>
                     {showModal && <ProjectModal name={name} showModal={showModal} setShowModal={setShowModal} images={images} handleClose={handleClose} tagList={tagList}></ProjectModal>}
